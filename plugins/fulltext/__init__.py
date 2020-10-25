855e02a4b5487c91b31a7c19c23901a1cd86e29f
larkwiot <ltfragor@gmail.com>
2020-10-25 14:37:44 -0400

    .

--- plugins/fulltext/__init__.py
+++ plugins/fulltext/__init__.py
@@ -1,1 +1,25 @@
-/home/v/fulltext.py
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import textract
+
+def get_text(file_path):
+    try:
+        return textract.process(file_path)
+    except:
+        return 'Failed to get full text'
+
+def add_mappings(mappings):
+    """Returns a dict with additional es mappings"""
+    mappings['mappings']['file']['properties'].update({
+        'fulltext': {
+            'type': "keyword"
+        }
+    })
+    return mappings
+
+def add_meta(file_path):
+    """Returns a dict with additional file meta data"""
+    text = get_text(file_path)
+    meta = {"fulltext": text}
+    return meta
